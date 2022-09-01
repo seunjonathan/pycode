cd567002d5d7c68dd7322f75e98f1e6f8d5a0c4e
Seun Jonathanm <seund@yahoo.com>
2022-08-31 20:49:36 -0600

    new one

---
+++ pycode.py
@@ -0,0 +1,9 @@
+import pandas as pd
+df = pd.read_csv("C:\\Users\\seunj\\Documents\\Datasets\\rccg\\csv\\May2022payment.csv")
+#print(df.head(3))
+df = df.drop(columns=['channel','gateway'])
+cols = list(df.columns.values)
+print(cols)
+#df.groupby('paymenttype')
+print(df.head())
+df.describe()
