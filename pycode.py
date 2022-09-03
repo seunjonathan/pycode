c4ed41601fc4f86130867aae5bc06c9cc7cc34ea
Seun Jonathanm <seund@yahoo.com>
2022-09-03 14:19:57 -0600

    today committed

--- pycode.py
+++ pycode.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
+import unittest
 df = pd.read_csv("C:\\Users\\seunj\\Documents\\Datasets\\rccg\\csv\\May2022payment.csv")
-#print(df.head(3))
+print(df.head(3))
 df = df.drop(columns=['channel','gateway'])
 cols = list(df.columns.values)
 print(cols)
